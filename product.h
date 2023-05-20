4fcd0a162641783478e1115b39f869eb8cf6dfc6
22001077 <130723447+22001077@users.noreply.github.com>
2023-05-21 03:05:33 +0900

    Update product.h

--- product.h
+++ product.h
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "product.h"
-#include <product.h>
+#include <time.h>
 
-//CRUD 구현(기본 기능 함수 구현)
+//CRUD 구현
 
+typedef struct boardContent{
+    char title[100];             // 게시글 제목
+    char contents[500];          // 게시글 내용
+    char writerName[30];        // 글쓴이 이름
+    char writtenDate[20];       // 작성된 시간
+} content;
+
+int selectMenu(); // 메뉴 선택 함수
+int addContent(); // 게시글 추가 함수
+void showContent(); // 추가된 게시글 호출 함수
+void listContent(); // 추가된 게시글의 내용 호출 함수
+void modifyContent(); // 게시글 수정 함수
+int cancelContent(); // 게시글 삭제 함수
+void saveData(); // 저장 함수
+int loadData(); // 불러오기 함수
