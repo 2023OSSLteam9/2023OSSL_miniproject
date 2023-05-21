cc8f9428112b529fe9eb88a7bc195c51c502e89f
lovmint <130054828+lovmint@users.noreply.github.com>
2023-05-22 03:43:25 +0900

    Update addfunc.h

--- addfunc.h
+++ addfunc.h
@@ -12,9 +12,9 @@
 int registerUser(const char *username, const char *password); // 회원가입
 int loginUser(const char *username, const char *password); // 로그인
 
-void addTransaction(const char* description, int miles); // 마일리지 부여
-void printTransactionHistory(); // 마일리지 점수 출력
-void awardMiles(int codingLevel); // 코딩레벨에 따른 마일리지 제공
+void addTransaction(const char* description, int miles); // 마일리지 제공
+void printTransactionHistory(); //마일리지 내역 출력
+void awardMiles(int codingLevel); // 입력된 레벨에 따른 마일리지 지급
 
 #endif /* ADDFUNC_H */
 
